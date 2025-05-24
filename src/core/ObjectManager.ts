97b0c4d34dd0ca5e1872e00510c872e6465c300d
YonbiDev <74678000+YonbiDev@users.noreply.github.com>
2025-05-24 19:47:36 +0200

    mise a jour

--- src/core/ObjectManager.ts
+++ src/core/ObjectManager.ts
@@ -1,3 +1,5 @@
+import { Turret } from "./Turret";
+
 export class ObjectManager {
     private scene: BABYLON.Scene;
     private objectTypes: { [key: string]: { size: number; color: BABYLON.Color3 } } = {
@@ -19,10 +21,7 @@ export class ObjectManager {
         if (!objectConfig) return;
 
         if (objectType === "turret") {
-            import("./Turret").then(module => {
-                const Turret = module.Turret;
-                new Turret(this.scene, position, 30,50,2000); // Default range is 100
-            });
+            new Turret(this.scene, position, 30, 50, 2000);
         } else {
             const object = BABYLON.MeshBuilder.CreateBox(objectType, { size: objectConfig.size }, this.scene);
             object.position = position;
